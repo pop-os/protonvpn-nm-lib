079bb8aad4213124b4802a25a836c83c778b3015
Alexandru Cheltuitor <31934100+calexandru2018@users.noreply.github.com>
2020-08-14 17:19:52 +0100

    Refactor how user manager uses default keyring servince and username

--- lib/services/user_manager.py
+++ lib/services/user_manager.py
@@ -5,14 +5,16 @@ from .user_session_manager import UserSessionManager
 
 
 class UserManager(UserSessionManager):
-    def __init__(self):
-        super().__init__()
-
-    def login(
-        self, username, password,
+    def __init__(
+        self,
         keyring_service=DEFAULT_KEYRING_SERVICE,
         keyring_username=DEFAULT_KEYRING_USERNAME
     ):
+        self.keyring_service = keyring_service
+        self.keyring_username = keyring_username
+        super().__init__()
+
+    def login(self, username, password):
         if not isinstance(username, str):
             raise TypeError(
                 "Incorrect object type, "
@@ -38,34 +40,19 @@ class UserManager(UserSessionManager):
         else:
             self.store_user_session(
                 session.dump(),
-                keyring_service,
-                keyring_username
+                self.keyring_service,
+                self.keyring_username
             )
 
-    def logout(
-        self,
-        keyring_service=DEFAULT_KEYRING_SERVICE,
-        keyring_username=DEFAULT_KEYRING_USERNAME
-    ):
-        if not keyring_service and not keyring_username:
-            keyring_service = self.DEFAULT_KEYRING_SERVICE
-            keyring_service = self.DEFAULT_KEYRING_USERNAME
-        self.delete_user_session(keyring_service, keyring_username)
+    def logout(self):
+        self.delete_user_session(self.keyring_service, self.keyring_username)
 
-    def fetch_vpn_credentials(
-        self,
-        keyring_service=DEFAULT_KEYRING_SERVICE,
-        keyring_username=DEFAULT_KEYRING_USERNAME
-    ):
-        session = self.load_session(keyring_service, keyring_username)
-
+    def fetch_vpn_credentials(self):
+        session = self.load_session()
         api_resp = session.api_request('/vpn')
-
         return (api_resp["VPN"]["Name"], api_resp["VPN"]["Password"])
 
-    def load_session(
-        self,
-        keyring_service=DEFAULT_KEYRING_SERVICE,
-        keyring_username=DEFAULT_KEYRING_USERNAME
-    ):
-        return self.load_stored_user_session(keyring_service, keyring_username)
+    def load_session(self):
+        return self.load_stored_user_session(
+            self.keyring_service, self.keyring_username
+        )
