1dff538fbdfc04b7ff1aab3d1101395dfd4e3d86
Alexandru Cheltuitor <31934100+calexandru2018@users.noreply.github.com>
2020-08-14 15:56:58 +0100

    Prepare test file

--- tests/test_user_manager.py
+++ tests/test_user_manager.py
@@ -1,15 +1,19 @@
 from lib.services.user_manager import UserManager
+from lib.services.user_manager import UserSessionManager
 from lib import exceptions
 import pytest
-# import string
-# import random
-# import keyring
-# import proton_python_client.proton as proton
-# import json
-# gi.require_version("NM", "1.0")
-# from gi.repository import NM
+import os
 
 
 class TestUserManager():
-
     cm = UserManager()
+
+    @pytest.fixture
+    def pvpn_user(self):
+        user = os.environ["vpntest_user"]
+        return user
+
+    @pytest.fixture
+    def pvpn_pass(self):
+        pwd = os.environ["vpntest_pwd"]
+        return pwd
