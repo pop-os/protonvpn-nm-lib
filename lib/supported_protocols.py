921647fe1a11407401a35244f978322bc0b2d53c
Alexandru Cheltuitor <31934100+calexandru2018@users.noreply.github.com>
2020-08-18 12:01:53 +0100

    Add supported enum of supported protocols

--- lib/supported_protocols.py
+++ lib/supported_protocols.py
@@ -1,5 +1,20 @@
-class SupportedProtocolsEnum(object):
-    TCP = "openvpn"
-    UDP = "openvpn"
-    # IKEV2 = "strongswan"
-    # WIREGUARD = "wireguard"
+class ProtocolEnum(object):
+    TCP = "tcp"
+    UDP = "udp"
+    IKEV2 = "ikev2"
+    WIREGUARD = "wireguard"
+
+
+class SupportedProtocolEnum(object):
+    OPENVPN = [
+        ProtocolEnum.TCP,
+        ProtocolEnum.UDP
+    ]
+    STRONGSWAN = ProtocolEnum.IKEV2
+    WIREGUARD = ProtocolEnum.WIREGUARD
+
+
+class ProtocolImplementationEnum(object):
+    OPENVPN = "openvpn"
+    STRONGSWAN = "strongswan"
+    WIREGUARD = "wireguard"
