2ea64fae06c3cc7ba38649c64ec7ba07d6b5f323
dhoko <dhoko@pm.me>
2020-08-21 11:26:54 +0200

    IT works

--- docker_entry.sh
+++ docker_entry.sh
@@ -3,31 +3,26 @@
 sudo /etc/init.d/dbus start && \
 sudo /etc/init.d/network-manager start
 
-echo "---1"
 # start gnome-keyring daemon at bash shell init and launch interactive dbus session
-#echo 'eval "$(printf '\\n' | gnome-keyring-daemon --unlock)"'  >> ~/.bashrc
+echo 'eval "$(printf '\\n' | gnome-keyring-daemon --unlock)"'  >> ~/.bashrc
 #echo 'eval "$(printf '\\n' | gnome-keyring-daemon -f )"'  >> $HOME/.profile
 #dbus-run-session -- bash
 
 #gnome-keyring-daemom --unlock;
 
 
-echo "---2"
 export $(dbus-launch)
-
-echo "---3"
-eval "$(dbus-launch --sh-syntax)"
-
-mkdir -p ~/.cache
-mkdir -p ~/.local/share/keyrings # where the automatic keyring is created
-
-echo "---4"
-# 1. Create the keyring manually with a dummy password in stdin
+#
+#eval "$(dbus-launch --sh-syntax)"
+#
+#mkdir -p ~/.cache
+#mkdir -p ~/.local/share/keyrings # where the automatic keyring is created
+#
+## 1. Create the keyring manually with a dummy password in stdin
 eval "$(printf '\n' | gnome-keyring-daemon --unlock)"
-
-echo "---5"
-# 2. Start the daemon, using the password to unlock the just-created keyring:
-eval "$(printf '\n' | /usr/bin/gnome-keyring-daemon --start)"
+#
+## 2. Start the daemon, using the password to unlock the just-created keyring:
+#eval "$(printf '\n' | /usr/bin/gnome-keyring-daemon --start)"
 
 
 if [ -f .env ]; then
