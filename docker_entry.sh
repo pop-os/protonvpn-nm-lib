329e2d36b184b171c0201e9d48e7068031d6dff0
dhoko <dhoko@pm.me>
2020-08-26 09:30:15 +0200

    Clean entrypoint and exec via arguments

--- docker_entry.sh
+++ docker_entry.sh
@@ -5,32 +5,18 @@ sudo /etc/init.d/network-manager start
 
 # start gnome-keyring daemon at bash shell init and launch interactive dbus session
 echo 'eval "$(printf '\\n' | gnome-keyring-daemon --unlock)"'  >> ~/.bashrc
-#echo 'eval "$(printf '\\n' | gnome-keyring-daemon -f )"'  >> $HOME/.profile
-#dbus-run-session -- bash
-
-#gnome-keyring-daemom --unlock;
-
 
 export $(dbus-launch)
-#
-#eval "$(dbus-launch --sh-syntax)"
-#
-#mkdir -p ~/.cache
-#mkdir -p ~/.local/share/keyrings # where the automatic keyring is created
-#
 ## 1. Create the keyring manually with a dummy password in stdin
 eval "$(printf '\n' | gnome-keyring-daemon --unlock)"
-#
-## 2. Start the daemon, using the password to unlock the just-created keyring:
-#eval "$(printf '\n' | /usr/bin/gnome-keyring-daemon --start)"
 
 
 if [ -f .env ]; then
-  echo 'load .env';
-  export $(cat .env | xargs)
+  echo 'find local .env ~ load new env';
+  export $(cat .env | xargs);
+  env;
 fi
 
-echo "---6"
 sudo bash -c  "cat <<EOT > /etc/polkit-1/localauthority/50-local.d/org.freedesktop.NetworkManager.pkla
 [nm-applet]
 Identity=unix-user:user
@@ -40,18 +26,4 @@ ResultInactive=no
 ResultActive=yes
 EOT"
 
-#eval `dbus-launch --sh-syntax`
-#eval `gnome-keyring-daemon --unlock`
-#export $(gnome-keyring-daemon -s -d | xargs);
-echo
-echo
-echo
-echo
-echo
-env;
-echo ""
-#dbus-run-session -- $@;
-#dbus-update-activation-environment --systemd DISPLAY
-#dbus-run-session bash -c "GNOME_KEYRING_CONTROL=1 $*";
-#$@;
-python3 -m pytest
+exec "$@";
