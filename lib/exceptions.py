a222be22835568ce33bf7aa0a51fc9059c2e9f20
Alexandru Cheltuitor <calexandru2018@pm.me>
2020-09-09 15:32:18 +0100

    Rename base exception

--- lib/exceptions.py
+++ lib/exceptions.py
@@ -1,96 +1,96 @@
-class CustomBaseException(BaseException):
+class ProtonVPNBaseException(BaseException):
     def __init__(self, message):
         self.message = message
-        super(CustomBaseException, self).__init__(self.message)
+        super(ProtonVPNBaseException, self).__init__(self.message)
 
 
-class AccessKeyringError(CustomBaseException):
+class AccessKeyringError(ProtonVPNBaseException):
     """Python-keyring error occured."""
 
 
-class AddConnectionCredentialsError(CustomBaseException):
+class AddConnectionCredentialsError(ProtonVPNBaseException):
     """Add credentials to connection error."""
 
 
-class IncorrectCredentialsError(CustomBaseException):
+class IncorrectCredentialsError(ProtonVPNBaseException):
     """Incorrect credentials error."""
 
 
-class APIAuthenticationError(CustomBaseException):
+class APIAuthenticationError(ProtonVPNBaseException):
     """Incorrect credentials error."""
 
 
-class ImportConnectionError(CustomBaseException):
+class ImportConnectionError(ProtonVPNBaseException):
     """Import connection configurations error."""
 
 
-class VirtualDeviceNotFound(CustomBaseException):
+class VirtualDeviceNotFound(ProtonVPNBaseException):
     """Virtual device could not be found."""
 
 
-class IllegalVirtualDevice(CustomBaseException):
+class IllegalVirtualDevice(ProtonVPNBaseException):
     """Unexpeced virtual device."""
 
 
-class AddConnectionFinishError(CustomBaseException):
+class AddConnectionFinishError(ProtonVPNBaseException):
     """Add connection finish error."""
 
 
-class StartConnectionFinishError(CustomBaseException):
+class StartConnectionFinishError(ProtonVPNBaseException):
     """Start connection finish error."""
 
 
-class StopConnectionFinishError(CustomBaseException):
+class StopConnectionFinishError(ProtonVPNBaseException):
     """Stop connection finish error."""
 
 
-class RemoveConnectionFinishError(CustomBaseException):
+class RemoveConnectionFinishError(ProtonVPNBaseException):
     """Remove connection finish error."""
 
 
-class IllegalVPNProtocol(CustomBaseException):
+class IllegalVPNProtocol(ProtonVPNBaseException):
     """Unexpexted plugin for specified protocol."""
 
 
-class ProtocolPluginNotFound(CustomBaseException):
+class ProtocolPluginNotFound(ProtonVPNBaseException):
     """Plugin for specified protocol was not found."""
 
 
-class ConnectionNotFound(CustomBaseException):
+class ConnectionNotFound(ProtonVPNBaseException):
     """ProtonVPN connection not found"""
 
 
-class ProtocolNotFound(CustomBaseException):
+class ProtocolNotFound(ProtonVPNBaseException):
     """Protocol not found upon generate certificate"""
 
 
-class IllegalAuthData(CustomBaseException):
+class IllegalAuthData(ProtonVPNBaseException):
     """Unexpected AuthData type"""
 
 
-class JSONAuthDataEmptyError(CustomBaseException):
+class JSONAuthDataEmptyError(ProtonVPNBaseException):
     """JSON AuthData empty error"""
 
 
-class JSONAuthDataNoneError(CustomBaseException):
+class JSONAuthDataNoneError(ProtonVPNBaseException):
     """JSON AuthData none error"""
 
 
-class JSONAuthDataError(CustomBaseException):
+class JSONAuthDataError(ProtonVPNBaseException):
     """JSON AuthData error"""
 
 
-class OptimumBackendNotFound(CustomBaseException):
+class OptimumBackendNotFound(ProtonVPNBaseException):
     """Optimum keyring backend not found"""
 
 
-class SessionError(CustomBaseException):
+class SessionError(ProtonVPNBaseException):
     """Session error"""
 
 
-class StoredSessionNotFound(CustomBaseException):
+class StoredSessionNotFound(ProtonVPNBaseException):
     """Stored session was not found"""
 
 
-class IllegalServername(CustomBaseException):
+class IllegalServername(ProtonVPNBaseException):
     """Unexpected servername"""
