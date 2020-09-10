1b9f1eba4ded723322983c22d6419e40a075601f
Alexandru Cheltuitor <calexandru2018@pm.me>
2020-09-10 12:55:18 +0100

    Add mehtod to validate username and password
    Add logging

--- lib/services/user_manager.py
+++ lib/services/user_manager.py
@@ -3,6 +3,7 @@ import proton
 from lib import exceptions
 from lib.constants import (APP_VERSION, DEFAULT_KEYRING_SERVICE,
                            DEFAULT_KEYRING_USERNAME)
+from lib.logger import logger
 
 from .user_session_manager import UserSessionManager
 
@@ -15,28 +16,21 @@ class UserManager(UserSessionManager):
     ):
         self.keyring_service = keyring_service
         self.keyring_username = keyring_username
+        logger.info(
+            "Initialized UserManager: service-> {}; user-> {}".format(
+                self.keyring_service, self.keyring_username
+            )
+        )
         super().__init__()
 
     def login(self, username, password):
-        """Login and store user session, with ProtonVPN credentials.
+        """Login ProtonVPN user.
 
         Args:
             username (string): ProtonVPN username
             password (string): ProtonVPN password
         """
-        if not isinstance(username, str):
-            raise TypeError(
-                "Incorrect object type, "
-                + "str is expected but got {} instead.".format(type(username))
-            )
-        elif not isinstance(password, str):
-            raise TypeError(
-                "Incorrect object type, "
-                + "str is expected but got {} instead.".format(type(password))
-            )
-        elif not username.strip() or not password.strip():
-            raise ValueError("Both username and password must be provided.")
-
+        self.validate_username_password(username, password)
         session = proton.Session(
             api_url="https://api.protonvpn.ch",
             appversion="LinuxVPN_" + APP_VERSION
@@ -45,6 +39,7 @@ class UserManager(UserSessionManager):
         try:
             session.authenticate(username, password)
         except proton.api.ProtonError as e:
+            logger.exception("[!] API ProtonError: {}".format(e))
             if e.code == 8002:
                 raise exceptions.IncorrectCredentialsError(e)
             else:
@@ -57,18 +52,46 @@ class UserManager(UserSessionManager):
             )
 
     def logout(self):
-        """Logout user."""
+        """Logout ProtonVPN user."""
         self.delete_user_session(self.keyring_service, self.keyring_username)
 
     def fetch_vpn_credentials(self, session=False):
-        """Fetch vpn credentials from api."""
+        """Fetch OpenVPN credentials from API."""
         if not session:
             session = self.load_session()
         api_resp = session.api_request('/vpn')
         return (api_resp["VPN"]["Name"], api_resp["VPN"]["Password"])
 
     def load_session(self):
-        """Load stored user session."""
+        """Load ProtonVPN user session."""
         return self.load_stored_user_session(
             self.keyring_service, self.keyring_username
         )
+
+    def validate_username_password(self, username, password):
+        """Validate ProtonVPN username and password.
+
+        Args:
+            username (string): ProtonVPN username
+            password (string): ProtonVPN password
+        """
+        if not isinstance(username, str):
+            err_msg = "Incorrect object type, "
+            + "str is expected but got {} instead.".format(type(username))
+            logger.error(
+                "[!] TypeError: {}. Raising exception.".format(err_msg)
+            )
+            raise TypeError(err_msg)
+        elif not isinstance(password, str):
+            err_msg = "Incorrect object type, "
+            + "str is expected but got {} instead.".format(type(password))
+            logger.error(
+                "[!] TypeError: {}. Raising exception.".format(err_msg)
+            )
+            raise TypeError(err_msg)
+        elif not username.strip() or not password.strip():
+            err_msg = "Both username and password must be provided."
+            logger.error(
+                "[!] ValueError: {}. Raising exception.".format(err_msg)
+            )
+            raise ValueError(err_msg)
