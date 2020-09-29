38e0fc67b2720c187222ce4ae452ea3753fff3ca
Alexandru Cheltuitor <calexandru2018@pm.me>
2020-09-29 11:39:51 +0100

    Update country to test

--- tests/test_server_manager.py
+++ tests/test_server_manager.py
@@ -217,7 +217,7 @@ class TestIntegrationServerManager:
             self.server_man.fastest(session, proto)
 
     def test_correct_generate_connect_country(self):
-        args = [["cc", "PT"]]
+        args = [["cc", "ES"]]
         servername, domain = self.server_man.country_f(
             REAL_SESSION, "tcp", *args
         )
@@ -249,7 +249,7 @@ class TestIntegrationServerManager:
             self.server_man.country_f(session, proto, *args)
 
     def test_correct_generate_connect_direct(self):
-        args = [["servername", "PT#5"]]
+        args = [["servername", "ES#5"]]
         servername, domain = self.server_man.direct(REAL_SESSION, "tcp", *args)
         resp = False
         if servername and domain:
@@ -257,7 +257,7 @@ class TestIntegrationServerManager:
         assert os.path.isfile(resp) is True
 
     def test_correct_generate_connect_direct_dialog(self):
-        args = ["PT#6"]
+        args = ["ES#6"]
         servername, domain = self.server_man.direct(REAL_SESSION, "tcp", *args)
         resp = False
         if servername and domain:
