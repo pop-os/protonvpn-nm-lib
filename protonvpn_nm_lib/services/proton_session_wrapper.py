9c1769bca8d68e7e298ac53892c1776edccbaa72
Alexandru Cheltuitor <calexandru2018@pm.me>
2020-10-23 19:08:10 +0100

    Refactor error handling
    For better error handling, only exceptions have to beed added for a generic error handling, while if a specific error handling has to be done, than a method has to be added, prefix with 'handle_' followed by the http status code.

--- protonvpn_nm_lib/services/proton_session_wrapper.py
+++ protonvpn_nm_lib/services/proton_session_wrapper.py
@@ -1,18 +1,24 @@
 from proton.api import ProtonError, Session
 from ..logger import logger
 from .. import exceptions
+import inspect
 
 
 class ProtonSessionWrapper():
     """Proton-client wrapper for improved error handling."""
     proton_session = False
+    API_ERROR_LIST = [
+        400, 401, 403, 404, 409,
+        422, 429, 500, 501, 503,
+    ]
+    API_EXCEPTION_DICT = {}
+    ERROR_CODE_HANDLER = {}
 
     def __init__(
         self, **kwargs
     ):
-        self.ERROR_CODE = {
-            401: self.handle_401,
-        }
+        self.setup_error_handling("handle_trivial_status")
+        self.setup_exception_handling()
         self.user_manager = kwargs.pop("user_manager")
         self.proton_session = Session(**kwargs)
 
@@ -39,7 +45,7 @@ class ProtonSessionWrapper():
             return api_response
 
         try:
-            result = self.ERROR_CODE[error.code](
+            result = self.ERROR_CODE_HANDLER[error.code](
                 error, args, **api_kwargs
             )
         except KeyError as e:
@@ -126,6 +132,10 @@ class ProtonSessionWrapper():
             return self.flatten_tuple(_tuple[0]) \
                 + self.flatten_tuple(_tuple[1:])
 
+    def handle_trivial_status(self, error):
+        logger.info("Catched {} error".format(error.code))
+        raise self.API_EXCEPTION_DICT[error.code](error.Error)
+
     def handle_401(self, error, *args, **kwargs):
         """Handles access token expiration."""
         logger.info("Catched 401 error")
@@ -140,3 +150,70 @@ class ProtonSessionWrapper():
         )
         logger.info("Calling api_request")
         return self.api_request(*args, **kwargs)
+
+    def handle_403(self, error, *args, **kwargs):
+        logger.info("Catched 403 error")
+        raise exceptions.API403Error(error)
+
+    def handle_429(self, error, *args, **kwargs):
+        logger.info("Catched 429 error")
+        raise exceptions.API429Error(error)
+
+    def handle_503(self, error, *args, **kwargs):
+        logger.info("Catched 504 error")
+        raise exceptions.API503Error(error)
+
+    def setup_error_handling(self, generic_handler_method_name):
+        """Setup automatic error handling.
+
+        All API handler methods should start with "handle_" and
+        then the number of the HTTP status that is to be handled.
+        The rest is taked care of by the code.
+
+        Args:
+            generic_handler_method_name (string):
+                name of the generic handler method
+        """
+        existing_handler_methods = {}
+        generic_handler_method = None
+        for class_member in inspect.getmembers(self):
+            if "handle_" in class_member[0]:
+                _, err_num, *_ = class_member[0].split("_")
+                try:
+                    err_num = int(err_num)
+                except ValueError:
+                    if generic_handler_method_name == class_member[0]:
+                        generic_handler_method = class_member[1]
+                else:
+                    if err_num in self.API_ERROR_LIST:
+                        existing_handler_methods[err_num] = class_member[1]
+
+        for err_num in self.API_ERROR_LIST:
+            if err_num not in existing_handler_methods:
+                self.ERROR_CODE_HANDLER[err_num] = generic_handler_method
+            else:
+                self.ERROR_CODE_HANDLER[err_num] = existing_handler_methods[err_num] # noqa
+
+    def setup_exception_handling(self):
+        """Setup automatic exception handling
+
+        This will search for exception that start with "API"
+        """
+        import re
+        for class_member in inspect.getmembers(exceptions):
+            if "API" in class_member[0]:
+                excp_name = class_member[0]
+                re_api_status_code = re.compile(
+                    r"^([A-Za-z]+)(\d+)([A-Za-z]+)$"
+                )
+                result = re_api_status_code.search(excp_name)
+                if not result:
+                    continue
+
+                err_num = result.groups()[1]
+
+                if err_num in self.API_ERROR_LIST:
+                    self.API_EXCEPTION_DICT[err_num] = class_member[1]
+                else:
+                    self.API_EXCEPTION_DICT[err_num] \
+                        = exceptions.UnhandledAPIError
