128586ab62133cba4dd2e3feeb961b75d3abe958
Alexandru Cheltuitor <calexandru2018@pm.me>
2020-10-27 18:16:29 +0000

    Separe CLI from library so that the latter is independent from the former

--- setup.py
+++ setup.py
@@ -5,34 +5,26 @@ from setuptools import find_packages, setup
 from protonvpn.constants import APP_VERSION
 
 long_description = """
-Contains the ProtonVPN NetworkManager Core + CLI
+Contains ProtonVPN NetworkManager library code for Linux clients.
 """
 
 setup(
-    name="protonvpn-cli",
+    name="protonvpn-nm-lib",
     version=APP_VERSION,
     packages=find_packages(),
-    entry_points={
-        "console_scripts": [
-            "protonvpn-cli = protonvpn.cli.cli:NetworkManagerPrototypeCLI"
-        ]
-    },
-    description="Proton Technologies Core/CLI",
+    description="ProtonVPN NetworkManager Linux library",
     author="Proton Technologies AG",
     author_email="contact@protonvpn.com",
     long_description=long_description,
     install_requires=[
         "proton-client", "pyxdg", "keyring",
-        "pythondialog", "PyGObject", "Jinja2",
-        "distro"
+        "PyGObject", "Jinja2", "distro"
     ],
     include_package_data=True,
     license="GPLv3",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Security",
-        "Environment :: Console",
-        "Intended Audience :: End Users/Desktop",
         "Operating System :: POSIX :: Linux",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3 :: Only",
