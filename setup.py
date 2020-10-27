3d40c11f5a5a71f182d1130dda856adc39c89d7c
Alexandru Cheltuitor <calexandru2018@pm.me>
2020-10-27 12:13:31 +0000

    Change package name and entry point
    Also update license, classifiers and author

--- setup.py
+++ setup.py
@@ -4,38 +4,42 @@ from setuptools import find_packages, setup
 
 from protonvpn.constants import APP_VERSION
 
-long_description = '''
+long_description = """
 Contains the ProtonVPN NetworkManager Core + CLI
-'''
+"""
 
 setup(
-    name='protonvpn-cli-experimental',
+    name="protonvpn-cli",
     version=APP_VERSION,
     packages=find_packages(),
     entry_points={
         "console_scripts": [
-            "protonvpn-exp = protonvpn.cli.cli:NetworkManagerPrototypeCLI"
+            "protonvpn-cli = protonvpn.cli.cli:NetworkManagerPrototypeCLI"
         ]
     },
-    description='Proton Technologies Core/CLI',
-    author='Proton Technologies',
-    author_email='contact@protonmail.com',
+    description="Proton Technologies Core/CLI",
+    author="Proton Technologies AG",
+    author_email="contact@protonmail.com",
     long_description=long_description,
     install_requires=[
-        'proton-client', 'pyxdg', 'keyring',
-        'pythondialog', 'PyGObject', 'Jinja2',
-        'distro'
+        "proton-client", "pyxdg", "keyring",
+        "pythondialog", "PyGObject", "Jinja2",
+        "distro"
     ],
     include_package_data=True,
-    license="MIT",
-    platforms="OS Independent",
+    license="GPLv3",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python',
-        'Topic :: Security',
+        "Development Status :: 3 - Alpha",
+        "Topic :: Security",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
