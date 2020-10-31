5eb74d8b7b2378e0c40be380a341d3cf680b0d58
Alexandru Cheltuitor <calexandru2018@pm.me>
2020-10-31 11:43:33 +0000

    Create new exceptions and new enum

--- protonvpn_nm_lib/exceptions.py
+++ protonvpn_nm_lib/exceptions.py
@@ -152,8 +152,16 @@ class ProtonSessionAPIError(ProtonSessionWrapperError):
     """Proton session API error."""
 
 
+class APIError(ProtonSessionWrapperError):
+    """API error."""
+
+
 class UnhandledAPIError(ProtonSessionWrapperError):
     """Unhandled API error."""
+
+
+class UnhandledAPIMethod(ProtonSessionWrapperError):
+    """Unhandled API method error."""
 
 
 
