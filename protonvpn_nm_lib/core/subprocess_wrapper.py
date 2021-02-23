bc6d805ba1d0f4cbf696c2b1c706236bc661aa8d
Alexandru Cheltuitor <calexandru2018@pm.me>
2021-02-23 10:59:15 +0000

    Apply changes to subprocess wrapper
    Upon initialization, all binaries have to be checked. If one of them is missing then an exception is going to be thrown.

--- protonvpn_nm_lib/core/subprocess_wrapper.py
+++ protonvpn_nm_lib/core/subprocess_wrapper.py
@@ -1,7 +1,5 @@
 import subprocess as _subprocess
 import os
-from pwd import getpwuid
-from grp import getgrgid
 
 
 class SubprocessWrapper():
@@ -19,89 +17,98 @@ class SubprocessWrapper():
         is effectivtly a layer on top of subprocess.
     """
 
-    __BIN_TO_SEARCH = ["nmcli", "systemctl", "clear"]
-    PIPE = -1
-    STDOUT = -2
-    DEVNULL = -3
+    _acceptable_binaries = {"nmcli", "systemctl", "clear"}
+    PIPE = _subprocess.PIPE
+    STDOUT = _subprocess.STDOUT
+    DEVNULL = _subprocess.DEVNULL
+
+    @staticmethod
+    def is_root_owned(path):
+        stat_info = os.stat(path)
+        return stat_info.st_uid == 0 and stat_info.st_gid == 0
 
     def __init__(self):
-        self.__PATHS_TO_SEARCH = []
-        self.__get_protected_paths()
+        self._path_to_binaries = {}
+        self.__search_for_matching_executables()
+        self.__ensure_executables_exist()
 
-    def __get_protected_paths(self):
-        """Gets only protected paths.
+    def __search_for_matching_executables(self):
+        """Searches for matching executables.
 
-        It looks at the uid and gid of a given folder
-        from PATH, and appends the folder to __PATHS_TO_SEARCH
-        given that both gid and uid of a folder are 0 (root).
+        While searching, it will attempt to match the
+        executables provided in _acceptable_binaries and ensure that
+        those executables are root owned.
+
+        It builds self._path_to_binaries, where
+        binary_short_name => full secure path.
         """
-        __PATHS_TO_SEARCH = os.environ["PATH"].split(":")
-        for path in __PATHS_TO_SEARCH:
-            stat_info = os.stat(path)
-            if stat_info.st_uid == 0 and stat_info.st_gid == 0:
-                self.__PATHS_TO_SEARCH.append(path)
+        # Look for root-owned directories in the system path in order
+        for path in os.environ.get('PATH', '').split(os.path.pathsep):
+            if not os.path.isdir(path):
+                continue
+            if not self.is_root_owned(path):
+                continue
 
-    def __check_call(self, args):
-        """Performs a security check.
+            # Check for all the binaries that we haven't matched yet
+            for binary in self._acceptable_binaries.difference(
+                self._path_to_binaries.keys()
+            ):
+                binary_path_candidate = os.path.join(path, binary)
+                if not os.path.isfile(binary_path_candidate):
+                    continue
 
-        This method takes in the args from run, and collects the
-        binaries that are specified in there, assuring that
-        only those binaries that are allowed in __BIN_TO_SEARCH
-        are to be searched for. If user provides any other type of executable
-        then the method will throw a LookupError.
+                if not self.is_root_owned(binary_path_candidate):
+                    continue
 
-        It then ensures that the file/binary is owned by root, by
-        invoking __ensure_file_is_owned_by_root.
+                # We're happy with that one, store it
+                self._path_to_binaries[binary] = binary_path_candidate
+
+            # Shortcut: if we don't have any executable left to find,
+            # we can exit the loop
+            if len(self._path_to_binaries) == len(self._acceptable_binaries):
+                break
+
+    def __ensure_executables_exist(self):
+        """Ensure that executables exist, by comparing the length of
+        self._path_to_binaries to self._acceptable_binaries.
         """
-        match_bin_collection = []
-        for bin_to_search in self.__BIN_TO_SEARCH:
-            if bin_to_search in args[0]:
-                match_bin_collection.append(bin_to_search)
-
-        if (match_bin_collection) == 0:
-            raise LookupError(
-                "No binaries were found, "
-                "please provide specific only the allowed binaries: {}".format(
-                    self.__BIN_TO_SEARCH)
+        # Were we unable to find executables? This is bad
+        if len(self._path_to_binaries) != len(self._acceptable_binaries):
+            missing_executables = self._acceptable_binaries.difference(
+                self._path_to_binaries.keys()
+            )
+            raise RuntimeError(
+                "Couldn't find acceptable "
+                "executables for {}".format(missing_executables)
             )
 
-        for path in self.__PATHS_TO_SEARCH:
-            for root, dirs, files in os.walk(path):
-                for matched_bin_name in match_bin_collection:
-                    if matched_bin_name in files:
-                        self.__ensure_file_is_owned_by_root(
-                            os.path.join(root, matched_bin_name)
-                        )
-
-    def __ensure_file_is_owned_by_root(self, filepath):
-        """Ensures that file is owned by root.
-
-        This method checks of the uid and gid of the file.
-        If both are 0, then the file belongs to root as should
-        be considered safe to use. Of course this does not cover
-        the situation where root could be compromised.
-        """
-        user_id = getpwuid(os.stat(filepath).st_gid).pw_uid
-        group_id = getgrgid(os.stat(filepath).st_gid).gr_gid
-
-        if group_id != 0 and user_id != 0:
-            raise Exception("Executable was not found")
-
     def run(
-        self, *popenargs, input=None, capture_output=False,
-        timeout=None, check=False, **kwargs
+        self, args, input=None, stdout=None, stderr=None,
+        capture_output=False, timeout=None, check=False
     ):
-        """Wrapper method for subprocess.run()."""
-        if len(popenargs) == 0:
-            raise TypeError("missing 1 required positional argument: 'args'")
+        """Wraps subprocess run.
 
-        self.__check_call(popenargs)
+        For security reason we limit the acceptable arguments here.
+        """
+        if (
+            type(args) != list
+            or len(args) < 1
+            or not all(type(a) == str for a in args)
+        ):
+            raise ValueError("args should be a non-empty list of string")
+
+        if args[0] not in self._path_to_binaries:
+            raise ValueError(
+                "{!r} is not an acceptable binary".format(args[0])
+            )
+
+        # Replace the path with the one we wanted
+        args[0] = self._path_to_binaries[args[0]]
+
         return _subprocess.run(
-            *popenargs,
-            capture_output=capture_output,
-            timeout=timeout,
-            check=check,
-            **kwargs
+            args, input=input, stdout=stdout,
+            stderr=stderr, capture_output=capture_output,
+            timeout=timeout, check=check
         )
 
 subprocess = SubprocessWrapper() # noqa
